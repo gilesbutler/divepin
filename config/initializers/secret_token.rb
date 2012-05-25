73bee9e8ef5d1c928aa06a9352c2cebc471d571f
Giles Butler <admin@gilesbutler.co.uk>
2012-05-25 15:25:07 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DivepinRails::Application.config.secret_token = 'a09536ecf7dd68b89264473613addd3cbe0219df72c332fd1de395dee00ec70a255a29ef97869c44b3af0ab08d8c87cf5e7e5a45432e6bc6a6db892df8f87f55'
